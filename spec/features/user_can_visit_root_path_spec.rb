d3467b5c54f80257498c26473234ca5c24a05e6d
James Belta <james.belta@gmail.com>
2021-01-08 10:17:18 -0700

    moved to search index_spec

--- spec/features/user_can_visit_root_path_spec.rb
+++ spec/features/user_can_visit_root_path_spec.rb
@@ -8,16 +8,4 @@ describe "User can visit root path" do
     expect(page).to have_content('Welcome to Poetry and Emotion')
     expect(page).to have_selector('input')
   end
-
-  it 'to search for an author' do
-    visit '/'
-
-    within '.search-containter' do
-      within '.search' do
-        fill_in 'author', with: 'shakespeare'
-        click_button 'Get Poems'
-        expect(current_path).to eq('/search')
-      end
-    end
-  end
 end
